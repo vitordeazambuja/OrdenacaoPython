9b0ac7ae60403c716557cbd8763def5b48a4b43d
Vitor <vitordeazambuja@edu.uniube.br>
2024-05-10 10:08:11 -0300

    Corrigido erro de estouro de pilha quick sort e adicionada arrays de tempo

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import time
 # Sorts
 # Quick Sort
 def particiona(V, inicio, final):
+    # Aleatoriza o pivo para nao ser chamado varias vezes e estourar a pilha
+    pivo_indice = random.randint(inicio,final)
+    V[inicio],  V[pivo_indice] = V[pivo_indice], V[inicio]
+    # Restante da lógica
     esq = inicio + 1
     dir = final
     pivo = V[inicio]
@@ -85,8 +89,43 @@ def shellSort(nums):
         h = int(h / 2.2)
     return nums
 
-# Definição dos tamanhos
+# Definição dos tamanhos e das arrays de tempos
 tamanhos = [100, 1000, 10000]
+
+# Tempos do Quick Sort
+temposCrescQuick = []
+temposDecrescQuick = []
+temposRandomQuick = []
+
+# Tempos do Merge Sort
+temposCrescMerge = []
+temposDecrescMerge = []
+temposRandomMerge = []
+
+# Tempos do Shell Sort
+temposCrescShell = []
+temposDecrescShell = []
+temposRandomShell = []
+
+
+# Funções de medir o tempo
+def medirTempoQuick(array):
+    tempo_inicial = time.time()
+    ordenado = quickSort(array, 0, len(array)-1)
+    tempo_final = time.time()
+    return tempo_final - tempo_inicial
+
+def medirTempoMerge(array):
+    tempo_inicial = time.time()
+    ordenado = mergeSort(array, 0, len(array)-1)
+    tempo_final = time.time()
+    return tempo_final - tempo_inicial
+
+def medirTempoShell(array):
+    tempo_inicial = time.time()
+    ordenado = shellSort(array)
+    tempo_final = time.time()
+    return tempo_final - tempo_inicial
 
 # for
 for tamanho in tamanhos:
@@ -94,3 +133,28 @@ for tamanho in tamanhos:
     array_cresc = list(range(tamanho))
     array_decresc = list(range(tamanho,0,-1))
     array_random = random.sample(range(tamanho*10),tamanho)
+    # Medição de tempo do quick sort
+    tempo_quick_cresc = medirTempoQuick(array_cresc)
+    tempo_quick_decresc = medirTempoQuick(array_decresc)
+    tempo_quick_random = medirTempoQuick(array_random)
+    # Append na array de tempos do quick sort
+    temposCrescQuick.append(tempo_quick_cresc)
+    temposDecrescQuick.append(tempo_quick_decresc)
+    temposRandomQuick.append(tempo_quick_random)
+    # Medição de tempo do merge sort
+    tempo_merge_cresc = medirTempoMerge(array_cresc)
+    tempo_merge_decresc = medirTempoMerge(array_decresc)
+    tempo_merge_random = medirTempoMerge(array_random)
+    # Append na array de tempos do merge sort
+    temposCrescMerge.append(tempo_merge_cresc)
+    temposDecrescMerge.append(tempo_merge_decresc)
+    temposRandomMerge.append(tempo_merge_random)
+    # Medição de tempo do shell sort
+    tempo_shell_cresc = medirTempoShell(array_cresc)
+    tempo_shell_decresc = medirTempoShell(array_decresc)
+    tempo_shell_random = medirTempoShell(array_random)
+    # Append na array de tempos do shell sort
+    temposCrescShell.append(tempo_shell_cresc)
+    temposDecrescShell.append(tempo_shell_decresc)
+    temposRandomShell.append(tempo_shell_random)
+    
